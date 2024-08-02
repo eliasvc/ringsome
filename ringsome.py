a249a0a93589ed7fff984d067c59d64e7ac39103
Elias Valdez <evaldez@gmail.com>
2024-08-01 23:08:32 -0400

    Add table printing to get countries

--- ringsome.py
+++ ringsome.py
@@ -1,5 +1,8 @@
 import requests
 import click
+from rich import print as rprint
+from rich.table import Table
+
 import countrycodes
 
 BASE_URL = "https://api.ring.nlnog.net/1.0"
@@ -20,8 +23,12 @@ def countries():
     r = requests.get(BASE_URL + "/countries")
     if r.status_code != 200:
         r.raise_for_status()
+    table = Table(title="Ring Countries")
+    table.add_column("Country Code", justify="center")
+    table.add_column("Country Name", justify="left")
     for code in r.json()["results"]["countrycodes"]:
-        print(f"{code}\t{countrycodes.alpha2[code]}")
+        table.add_row(code, countrycodes.alpha2[code])
+    rprint(table)
 
 
 @get.command()
